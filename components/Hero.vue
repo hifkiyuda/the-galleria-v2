1df7f8fbc643752d6f2054a50a7dc6abebb2d18b
hifkiyuda <hifkiyudapratama001@gmail.com>
2023-08-11 20:14:12 +0700

    Initial commit

---
+++ components/Hero.vue
@@ -0,0 +1,26 @@
+<template>
+  <div class="container m-auto mx-16 w-auto font-aboreto">
+    <div class="relative h-screen">
+      <div class="h-full w-full bg-cover bg-center" style="background-image: url('https://artincontext.org/wp-content/uploads/2022/04/Liberty-Leading-the-People-by-Euge%CC%80ne-Delacroix-848x530.avif');">
+        <div class="absolute inset-0 flex items-center bg-black bg-opacity-50">
+          <div class="text-white ml-16">
+            <h1 class="text-8xl">The Galleria.</h1>
+            <p class="mt-4 text-2xl">World Famous Paintings.</p>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script setup>
+
+</script>
+
+<style scoped>
+@import url('https://fonts.googleapis.com/css2?family=Aboreto&display=swap');
+
+.font-aboreto {
+  font-family: 'Aboreto', cursive;
+}
+</style>
---
+++ components/Navbar.vue
@@ -0,0 +1,13 @@
+<template>
+  <header class="container m-auto py-6 px-16 flex flex-row justify-between items-center font-['Lucida_Sans']">
+    <NuxtLink class="flex flex-row items-center">
+      <div class="font-light text-2xl">The Galleria |</div>
+      <img class="h-7 ml-2" src="../assets/images/hyp-logo-black.png" />
+    </NuxtLink>
+    <nav class="flex flex-row justify-between items-center uppercase text-base font-light">
+      <NuxtLink class="p-2 ml-10 hover:cursor-pointer">Home</NuxtLink>
+      <NuxtLink class="p-2 ml-10 hover:cursor-pointer">Paintings</NuxtLink>
+      <NuxtLink class="p-2 ml-10 hover:cursor-pointer">Artists</NuxtLink>
+    </nav>
+  </header>
+</template>
